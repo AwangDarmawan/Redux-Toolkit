7288ae037ac11f1626a384d25f9ef678c90c0291
AwangDarmawan <adarmawan468@gmail.com>
2024-12-02 10:38:23 +0700

    deploy json

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'http://localhost:3000/';
+const API_URL =  import.meta.env.VITE_BASE_URL;
 export const fetchData = async () => {
         const response = await axios.get(`${API_URL}products`);
         return response.data;
--- src/component/AddProduct.jsx
+++ src/component/AddProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { CreateDataAsync } from '../features/ProductSlice';
-import { Navigate, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
 
 const AddProduct = () => {
